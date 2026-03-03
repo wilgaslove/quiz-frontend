d16da6f8cb72d732904f3c170e8b235894c5ea08
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-03-03 18:23:37 +0100

    tailwindc

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Login from "@/views/Login.vue"
+import Dashboard from "@/views/Dashboard.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,12 @@ const router = createRouter({
     {
       path: "/login",
       component: Login
+      
+    },
+    {
+      path:"/",
+      component: Dashboard,
+      meta: {requiresAuth: true}
     }
   ]
 })
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const user = localStorage.getItem("user")
+
+const logout = () => {
+  localStorage.removeItem("token")
+  localStorage.removeItem("user")
+  router.push("/login")
+}
+</script>
+
+<template>
+  <div class="dashboard">
+    <h2>Dashboard</h2>
+
+    <p v-if="user">
+      Connecté en tant que :
+      <strong>{{ JSON.parse(user).email }}</strong>
+    </p>
+
+    <button @click="logout">Déconnexion</button>
+  </div>
+</template>
+
+<style scoped>
+.dashboard {
+  max-width: 400px;
+  margin: 80px auto;
+}
+button {
+  padding: 10px;
+  background: black;
+  color: white;
+  border: none;
+}
+</style>
