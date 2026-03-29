fba8b6b6433a9681c7b8d4074219ec9b662ee6ff
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-03-29 23:36:13 +0100

    ded

---
+++ src/views/Result copy.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div>
+
+        <h1>Résultat du Quiz</h1>
+
+        <div v-if="result">
+            <p>Score : {{ result.score }} / {{ result.total }}</p>
+            <p>{{ result.message }}</p>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+
+    data() {
+        return {
+            result: null
+        }
+    },
+
+    async mounted() {
+
+        const res = await fetch(`http://localhost:5000/api/results/my-results`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+
+        const data = await res.json()
+
+        this.result = data[data.length - 1] // dernier quiz
+
+    }
+
+}
+
+</script>
