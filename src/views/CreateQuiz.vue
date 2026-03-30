ee5138a97e96edb37dd6261ebc1af992b871c0d1
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-03-30 21:25:27 +0200

    C

---
+++ src/views/CreateQuiz.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import api from "@/services/api"
+
+const router = useRouter()
+
+const title = ref("")
+const description = ref("")
+const duration = ref(60)
+
+const createQuiz = async () => {
+  await api.post("/quiz", {
+    title: title.value,
+    description: description.value,
+    duration: duration.value,
+    questions: []
+  })
+
+  router.push("/admin")
+}
+</script>
+
+<template>
+  <div>
+    <h1>Créer un Quiz</h1>
+
+    <input v-model="title" placeholder="Titre" />
+    <input v-model="description" placeholder="Description" />
+    <input v-model="duration" type="number" placeholder="Durée" />
+
+    <button @click="createQuiz">Créer</button>
+  </div>
+</template>
