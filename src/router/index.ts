c6356c7420d2d550b7d0ec5241d14b8a4bc4fc9b
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-04-16 18:25:52 +0200

    structure de la page copie router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Login from '@/views/Login.vue'
 import Dashboard from '@/views/Dashboard.vue'
 import QuizPlay from '@/views/QuizPlay.vue'
-import Result from '@/views/Result.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: 'admin/dashboard',
+      redirect: '/dashboard',
     },
+
     {
       path: '/login',
       component: Login,
     },
+
+    // 👤 USER
     {
       path: '/dashboard',
       component: Dashboard,
@@ -26,38 +29,21 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
 
+    // 👨‍💼 ADMIN
     {
-  path: "/admin",
-  component: () => import("@/views/AdminQuiz.vue")
-},
-{
-  path: "/admin/create",
-  component: () => import("@/views/CreateQuiz.vue")
-},
-{
-  path: "/admin/edit/:id",
-  component: () => import("@/views/EditQuiz.vue")
-}
-
-    // afichge des résultats
-    // {
-    //   path: '/result/:quizId',
-    //   name: 'Result',
-    //   component: Result,
-    // },
+      path: '/admin',
+      component: () => import('@/views/AdminQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true }
+    },
+    {
+      path: '/admin/create',
+      component: () => import('@/views/CreateQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true }
+    },
+    {
+      path: '/admin/edit/:id',
+      component: () => import('@/views/EditQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true }
+    },
   ],
 })
-
-
-
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token')
-
-  if (to.meta.requiresAuth && !token) {
-    next('/login')
-  } else {
-    next()
-  }
-})
-
-export default router
