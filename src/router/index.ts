10bf7b120a44c0d8983320e20519c6cab3943f42
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-04-17 20:17:17 +0200

    correction des routes suite à la nouvelle structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '@/views/Login.vue'
-import Dashboard from '@/views/Dashboard.vue'
-import QuizPlay from '@/views/QuizPlay.vue'
+import Dashboard from '@/views/admin/Dashboard.vue'
+import QuizPlay from '@/views/admin/QuizPlay.vue'
+
+// const Dashboard = () => import('@/views/admin/Dashboard.vue')
+// const QuizPlay = () => import('@/views/user/QuizPlay.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,18 +35,21 @@ const router = createRouter({
     // 👨‍💼 ADMIN
     {
       path: '/admin',
-      component: () => import('@/views/AdminQuiz.vue'),
-      meta: { requiresAuth: true, adminOnly: true }
+      component: () => import('@/views/admin/AdminQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true },
     },
     {
       path: '/admin/create',
-      component: () => import('@/views/CreateQuiz.vue'),
-      meta: { requiresAuth: true, adminOnly: true }
+      component: () => import('@/views/admin/CreateQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true },
     },
     {
       path: '/admin/edit/:id',
-      component: () => import('@/views/EditQuiz.vue'),
-      meta: { requiresAuth: true, adminOnly: true }
+      component: () => import('@/views/admin/EditQuiz.vue'),
+      meta: { requiresAuth: true, adminOnly: true },
     },
   ],
 })
+
+
+export default router;
