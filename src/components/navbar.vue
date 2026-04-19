868559fe8d84dbf99622161dbed03af17bb877aa
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-04-19 07:58:53 +0100

    correction de navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,13 +2,13 @@
 
 </script>
 <template>
-    <!-- <nav>
+    <nav>
         <h1>Quiz App</h1>
 
         <div class="flex justify-between" style="gap: 20px">
-            <router-link to="/" class="btn btn-primary"²>Accueil</router-link>
+            <router-link to="/" class="btn btn-primary">Accueil</router-link>
             <router-link to="/quizzes" class="btn btn-secondary">Quizzes</router-link>
             <router-link to="/admin" class="btn btn-danger">Admin</router-link>
         </div>
-    </nav> -->
+    </nav>
 </template>
