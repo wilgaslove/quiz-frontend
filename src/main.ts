d23d9f896b0c3805dfd305b41f1caaf0d495ab28
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-04-27 18:15:37 +0200

    correction login main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
+const auth = useAuthStore()
+auth.init() // Initialiser l'état de l'authentification à partir du localStorage
 
 app.use(createPinia())
 app.use(router)
