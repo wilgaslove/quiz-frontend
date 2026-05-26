1040915e0d726f62e584a8e27c0682a27ce5b12e
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-26 19:25:54 +0200

    navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -6,8 +6,8 @@
         <!-- <h1>Quiz App</h1> -->
 
         <div class="bg-red-500" style="gap: 20px">
-            <router-link to="/" class="btn btn-primary">Accueil</router-link>
-            <router-link to="/quizzes" class="btn btn-secondary">Quizzes</router-link>
+            <router-link to="/" class="btn btn-primary text-red-500">Accueil</router-link>
+            <router-link to="/admin/create" class="btn btn-secondary">Créer un quiz</router-link>
             <router-link to="/admin" class="btn btn-danger">Admin</router-link>
         </div>
     </nav>
