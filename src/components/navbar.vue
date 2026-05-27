a7cb33e1d3fc61f8e8ff562335899521f0fcc0d1
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-27 20:09:15 +0200

    mis en forme navbare

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,7 +5,7 @@
     <nav >
         <!-- <h1>Quiz App</h1> -->
 
-        <div  class="flex justify-center gap-[80px] p-4 bg-gray-800 text-white ">
+        <div  class="flex justify-center gap-[80px] p-4 border-double border-4 border-gray-300 rounded-lg shadow-md w-full bg-gray-800 text-white ">
             <router-link to="/" class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-lg">Accueil</router-link>
             <router-link to="/admin/create" class="transition ease-in-out delay-150 bg-green-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-600 duration-300 px-4 py-2 rounded-lg">Créer un Quiz</router-link>
             <router-link to="/admin" class="transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-600 duration-300 px-4 py-2 rounded-lg">Admin</router-link>
