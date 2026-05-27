64bb3a6a4d4ca45cccc740799e43191cc87c42b2
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-27 19:12:35 +0200

    mis en forme quiz

--- src/views/user/Dashboard.vue
+++ src/views/user/Dashboard.vue
@@ -24,15 +24,14 @@ onMounted(() => {
 <template>
 
   <div class="dashboard">
-    <h1>Dashboard apprenant</h1>
-    <h2>Liste des Quiz</h2>
+    <h1 class="text-2xl font-bold mb-4">Liste des Quiz</h1>
 
     <p v-if="quizStore.loading">Chargement...</p>
 
-    <div v-for="quiz in quizStore.quizzes" :key="quiz._id" class="quiz-card">
-      <h3>{{ quiz.title }}</h3>
-      <p>{{ quiz.description }}</p>
-      <p>Durée : {{ quiz.duration }} secondes</p>
+    <div v-for="quiz in quizStore.quizzes" :key="quiz._id" class="quiz-card rounded-lg">
+      <h3 class="text-lg font-semibold">{{ quiz.title }}</h3>
+      <p class="text-gray-600">{{ quiz.description }}</p>
+      <p class="text-lg font-semibold">Durée : {{ quiz.duration }} secondes</p>
 
       <button @click="startQuiz(quiz._id)" class="transition ease-in-out delay-150 bg-green-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-600 duration-300 px-4 py-2 text-white border-double border-4 rounded-lg">Démarrer</button>
     </div>
