4bb57f01b41050a41a0838fe7e41766a12e6e959
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-27 20:32:56 +0200

    mis en forme dashbord admin

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -24,27 +24,27 @@ onMounted(loadStats)
 
 <template>
   <navbar />
-  <div class="p-6">
+  <div class="p-6 max-w-4xl mx-auto">
 
     <!-- STATS -->
     <div v-if="stats" class="grid">
-      <div class="card">
-        <h3>Quiz</h3>
+      <div class="card ">
+        <h3 class="text-lg font-semibold">Quiz</h3>
         <p>{{ stats.totalQuizzes }}</p>
       </div>
 
       <div class="card">
-        <h3>Utilisateurs</h3>
+        <h3 class="text-lg font-semibold">Utilisateurs</h3>
         <p>{{ stats.totalUsers }}</p>
       </div>
 
       <div class="card">
-        <h3>Participations</h3>
+        <h3 class="text-lg font-semibold">Participations</h3>
         <p>{{ stats.totalResults }}</p>
       </div>
 
       <div class="card">
-        <h3>Score moyen</h3>
+        <h3 class="text-lg font-semibold">Score moyen</h3>
         <p>{{ stats.avgScore.toFixed(2) }}</p>
       </div>
     </div>
@@ -52,19 +52,19 @@ onMounted(loadStats)
     <!-- LEADERBOARD -->
     <h2 style="margin-top:30px">🏆 Top joueurs</h2>
 
-    <table border="1" cellpadding="10">
+    <table border="1" cellpadding="10" class="w-full mt-4 text-left"  style="border-collapse: collapse;">
       <thead>
         <tr>
-          <th>Nom</th>
-          <th>Score total</th>
-          <th>Quiz passés</th>
+          <th class="border px-4 py-2">Nom</th>
+          <th class="border px-4 py-2">Score total</th>
+          <th class="border px-4 py-2">Quiz passés</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="user in leaderboard" :key="user.userId">
-          <td>{{ user.nom }}</td>
-          <td>{{ user.totalScore }}</td>
-          <td>{{ user.totalQuizzes }}</td>
+          <td class="border px-4 py-2">{{ user.nom }}</td>
+          <td class="border px-4 py-2 text-right">{{ user.totalScore }}</td>
+          <td class="border px-4 py-2 text-right">{{ user.totalQuizzes }}</td>
         </tr>
       </tbody>
     </table>
