31e7cfb49b260eed284d81ddeb5edca2ffc39d7c
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-05-28 18:58:27 +0200

    bouton deconnexion

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,13 +2,20 @@
 
 </script>
 <template>
-    <nav >
+    <nav>
         <!-- <h1>Quiz App</h1> -->
 
-        <div  class="flex justify-center gap-[80px] p-4 border-double border-4 border-gray-300 rounded-lg shadow-md w-full bg-gray-800 text-white ">
-            <router-link to="/" class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-lg">Accueil</router-link>
-            <router-link to="/admin/create" class="transition ease-in-out delay-150 bg-green-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-600 duration-300 px-4 py-2 rounded-lg">Créer un Quiz</router-link>
-            <router-link to="/admin" class="transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-600 duration-300 px-4 py-2 rounded-lg">Admin</router-link>
+        <div
+            class="flex justify-center gap-[80px] p-4 border-double border-4 border-gray-300 rounded-lg shadow-md w-full bg-gray-800 text-white ">
+            <router-link to="/"
+                class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-lg">Accueil</router-link>
+            <router-link to="/admin/create"
+                class="transition ease-in-out delay-150 bg-green-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-600 duration-300 px-4 py-2 rounded-lg">Créer
+                un Quiz</router-link>
+            <router-link to="/admin"
+                class="transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-600 duration-300 px-4 py-2 rounded-lg">Admin</router-link>
+            <router-link to="/login"
+                class="transition ease-in-out delay-150 bg-yellow-500 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-600 duration-300 px-4 py-2 rounded-lg">Déconnecter</router-link>
         </div>
     </nav>
 </template>
