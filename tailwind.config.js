70565bde4170ce8322e2f80579325ed26c90671e
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-06-03 22:52:36 +0200

    darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
