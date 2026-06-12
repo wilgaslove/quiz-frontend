f2d4a08162d438d3e941d8078b2b89f82f3c0fd8
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-06-12 16:55:40 +0200

    changement de code affichage

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -11,7 +11,7 @@ const authStore = useAuthStore()
         <div
         class="flex justify-center fixed top-0 gap-[80px] p-4 border-double border-4 border-gray-300 rounded-lg shadow-md w-full text-white  ">
 
-        <div class="fixed left-4 text-lg font-bold text-black"> Welcom {{ authStore.user?.nom }} </div>
+        <!-- <div class="fixed left-4 text-lg font-bold bg-gray-800 text-white p-2 rounded-lg"> Welcom {{ authStore.user?.nom }} </div> -->
 
         <router-link to="/"
                 class="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-4 py-2 rounded-lg">Liste
