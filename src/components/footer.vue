7b190330237a3d6f5ed1e5acc6785650e5591d82
Wilgas ADJOVI <wilfrieda216@gmail.com>
2026-06-13 21:23:26 +0200

    changement de code affichage

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
 <template>
   <footer class=" bg-gray-800 text-white py-4 text-center">
     <div class=" container mx-auto px-4">
-      <p class=" ">&copy; 2026 Quiz App. Tous droits réservés.</p>
+      <p class=" ">&copy; 2026 LogInno Tech. Tous droits réservés.</p>
     </div>
   </footer>
 </template>
